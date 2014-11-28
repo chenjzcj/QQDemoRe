18d152d00a4882d2aa4de83b82ed2205490deb47
chenjzcj <527633405@qq.com>
2014-11-28 16:22:53 +0800

    uu

--- QQDemo/src/com/succ7/qqdemo/MainActivity.java
+++ QQDemo/src/com/succ7/qqdemo/MainActivity.java
@@ -10,13 +10,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		System.out.println("");
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
-
 }
